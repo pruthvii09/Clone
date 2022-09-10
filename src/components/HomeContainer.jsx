9778781548e763e79d4c357e1379c5512750656f
Pruthviraj Auti <ytvancedpruthvi@gmail.com>
2022-09-10 14:40:33 +0530

    home container

---
+++ src/components/HomeContainer.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import Delivery from "../img/delivery.png";
+import HeroBg from "../img/heroBg.png";
+import { heroData } from "../utils/Data";
+
+const HomeContainer = () => {
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-5 w-full" id="home">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+          <p className="text-base text-orange-500 font-semibold">
+            Bike Delivery
+          </p>
+          <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
+            <img
+              className="w-full h-full object-contain"
+              src={Delivery}
+              alt=""
+            />
+          </div>
+        </div>
+
+        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+          The Fastest Delivery in
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
+            Your City
+          </span>
+        </p>
+
+        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quo
+          assumenda, ea nesciunt quisquam iusto et dicta dolore dolorum
+          voluptatem earum! Veritatis quasi explicabo, hic expedita
+          necessitatibus quis numquam repudiandae?
+        </p>
+
+        <button
+          type="button"
+          className=" md:w-auto  bg-gradient-to-br from-orange-400 to-orange-500 w-full px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+        >
+          Order Now
+        </button>
+      </div>
+      <div className="py-2 flex-1 flex items-center relative">
+        <img
+          src={HeroBg}
+          className="ml-auto h-420 w-full lg:w-auto lg:h-650 "
+          alt="herobg"
+        />
+        <div className="w-80% h-full absolute top-0 right-[4.5rem] flex gap-6 flex-wrap items-center justify-center py-4">
+          {heroData &&
+            heroData.map((n) => (
+              <div
+                key={n.id}
+                className="lg:w-190 p-4 bg-cardOverlay drop-shadow-lg backdrop-blur-md rounded-3xl flex flex-col items-center justify-center "
+              >
+                <img
+                  src={n.imgSrc}
+                  className="w-20 lg:w-40 -mt-10 lg:-mt-20"
+                  alt=""
+                />
+                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {n.name}
+                </p>
+                <p className="text-[10px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {n.decp}
+                </p>
+                <p className="text-sm font-semibold">
+                  <span className="text-xs text-red-600">₹</span> {n.price}
+                </p>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeContainer;
---
+++ src/utils/Data.js
@@ -0,0 +1,35 @@
+import I1 from "../img/i1.png";
+import F1 from "../img/f1.png";
+import C3 from "../img/c3.png";
+import Fi1 from "../img/fi1.png";
+
+export const heroData = [
+    {
+        id: 1,
+        name: "Icecream",
+        decp: "Chocolate & Vanilla",
+        price: "105",
+        imgSrc: I1,
+    },
+    {
+        id: 2,
+        name: "Strawberries",
+        decp: " Fresh Strawberries",
+        price: "250 ",
+        imgSrc: F1,
+    },
+    {
+        id: 3,
+        name: "Chicken Kabab",
+        decp: "Mixed Kabab Plate",
+        price: "150",
+        imgSrc: C3,
+    },
+    {
+        id: 4,
+        name: "Fish Kabab",
+        decp: "Chocolate & Vanilla",
+        price: "225",
+        imgSrc: Fi1,
+      },
+  ];
